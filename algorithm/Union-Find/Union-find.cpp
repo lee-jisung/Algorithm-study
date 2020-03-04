a4d83d37d53aecd248411d9ffd82d73704f0f8d5
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-03-04 21:26:13 +0900

    Update Union-find.cpp

--- algorithm/Union-Find/Union-find.cpp
+++ algorithm/Union-Find/Union-find.cpp
@@ -13,6 +13,7 @@
 using namespace std;
 
 int n, m; 
+int parent[SIZE];
 
 /*
    Disjoint-set
@@ -25,36 +26,35 @@ int n, m;
 */
 
 // 부모 노드를 찾는 함수
-int getParent(int parent[], int x) {
+int getParent(int x) {
     if (parent[x] == x) return x;
-    return parent[x] = getParent(parent, parent[x]);
+    return parent[x] = getParent(parent[x]);
 }
 
 // 두 노드를 합치는 함수 (가장 작은 수를 부모노드로 갖음)
-void unionParent(int parent[], int a, int b) {
-    a = getParent(parent, a);
-    b = getParent(parent, b);
+void unionParent(int a, int b) {
+    a = getParent(a);
+    b = getParent(b);
     if (a < b) parent[b] = a;
     else parent[a] = b;
 }
 
 // 같은 부모를 가지는지 확인
-int findParent(int parent[], int a, int b) {
-    a = getParent(parent, a);
-    b = getParent(parent, b);
+int findParent(int a, int b) {
+    a = getParent(a);
+    b = getParent(b);
     if (a == b) return 1;
     return 0;
 }
 
 int main(void) {
 
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
     int a, b;
     cin >> a >> b;
-
-    int parent[11];
+	
     for (int i = 1; i <= 10; i++) {
         parent[i] = i;
     }
