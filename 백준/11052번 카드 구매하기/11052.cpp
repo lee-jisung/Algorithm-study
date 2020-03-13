a7e52322c39a2d59b0a0cc366e1d856925624e66
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-03-13 20:54:43 +0900

    DP

--- 백준/11052번 카드 구매하기/11052.cpp
+++ 백준/11052번 카드 구매하기/11052.cpp
@@ -17,6 +17,14 @@ long long dp[SIZE], pay[SIZE];
 // dp[n] = max(dp[n], dp[n-i] + p[i]) => i= 1 ~ n까지
 // 매번 최적화된 답을 dp[n]에 저장
 
+/*
+   마지막에 1개짜리 카드팩 산다 --> n-1개의 카드 구매 + 1개 짜리 카드팩 ==> dp[n-1] + pay[1]
+   마지막에 2개 짜리 카드팩 --> n-2개 카드 구매 + 2개짜리 카드팩 ==> dp[n-2] + pay[2]
+   ...
+   n개짜리 카드팩 --> dp[n-n] + pay[n] 
+   의 모든 경우를 dp[1 ~ n]까지 구해주면 됨
+*/
+
 int main(void) {
 
 	cin.tie(NULL);
