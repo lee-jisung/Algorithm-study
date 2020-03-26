13e4b5b981ca0c6deab219c7325dd67f8cda063d
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-03-26 12:07:38 +0900

    inchworm algorithm (two pointer)

---
+++ 백준/1806번 부분합/1806.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <vector>
+#include <deque>
+#include <cmath>
+
+#define SIZE 100001
+#define INF 987654321
+
+using namespace std;
+
+int n, s, front, rear, res, sum;
+int a[SIZE];
+
+int main(void) {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> s;
+
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	res = INF;
+	sum = a[0];
+
+	while (front < n && rear < n) {
+
+		if (sum < s) {
+			rear++;
+			sum += a[rear];
+			continue;
+		}
+		if (sum >= s) {
+			res = min(res, (rear - front) + 1);
+			sum -= a[front];
+			front++;
+		}
+	}
+	if (res == INF) cout << 0 << "\n";
+	else cout << res << "\n";
+
+	return 0;
+}
