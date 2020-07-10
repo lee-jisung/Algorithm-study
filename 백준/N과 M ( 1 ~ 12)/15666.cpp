cf70ccf58332b86606940c3843bc0c115bdc6367
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-07-10 14:39:01 +0900

    N과 M (12)

---
+++ 백준/N과 M ( 1 ~ 12)/15666.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int n, m, arr[9];
+vector<int> num;
+map<int, int>mp;
+
+void func(int no) {
+
+	if (no == m) {
+		bool ck = true;
+		for(int i=0; i<m-1; i++)
+			if (arr[i] > arr[i + 1]) {
+				ck = false;
+				break;
+			}
+		if (!ck) return;
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < num.size(); i++) {
+		arr[no] = num[i];
+		func(no + 1);
+	}
+}
+
+int main(void) {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int tmp; cin >> tmp;
+		if (!mp.count(tmp)) {
+			mp[tmp] = 1;
+			num.push_back(tmp);
+		}
+		else {
+			mp[tmp]++;
+		}
+	}
+	sort(num.begin(), num.end());
+
+	func(0);
+
+	return 0;
+}
