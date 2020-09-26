0cd20ad1dab28e4620e21f12ad0feeeff697a799
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-09-26 23:10:10 +0900

    DP

--- 백준/1344번 축구/1344.cpp
+++ 백준/1344번 축구/1344.cpp
@@ -5,22 +5,20 @@ using namespace std;
 
 /*
    90분 => 5분 간격 => 18 간격
-   dp[현재][a골수][b골수] 
-
+   dp[현재][a골수][b골수]
    1. a 골 o, b골 o => dp[n][a][b] = a * b * dp[n-1][a-1][b-1]  // a-1골수, b-1골수에서 둘 다 넣었으니 a, b확률 곱한것
    2. a 골 x, b골 o => dp[n][a][b] = (1-a) * b * dp[n-1][a][b-1]
    3. a 골 o, b골 x => dp[n][a][b] = a * (1 - b) * dp[n-1][a-1][b]
    4. a 골 x, b골 x => dp[n][a][b] = (1-a) * (1-b) * dp[n-1][a-1][b-1]
-
  */
 
-double a, b, result;
+double A, B, result;
 double dp[19][19][19];
 
-bool isPrime(int n) {
-	if (n == 0 || n == 1) return false;
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (n % i == 0) return false;
+bool isPrime(int x) {
+	if (x == 0 || x == 1) return false;
+	for (int i = 2; i <= sqrt(x); i++) {
+		if (x % i == 0) return false;
 	}
 	return true;
 }
@@ -28,39 +26,37 @@ bool isPrime(int n) {
 int main(void) {
 
 	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-	cin >> a >> b;
-	a /= 100; b /= 100;
+	cin >> A >> B;
+	A /= 100; B /= 100;
 
-	dp[1][1][1] = a * b;
-	dp[1][0][1] = (1 - a) * b;
-	dp[1][1][0] = a * (1 - b);
-	dp[1][0][0] = (1 - a) * (1 - b);
+	dp[1][1][1] = A * B ;
+	dp[1][1][0] = A * (1 - B);
+	dp[1][0][1] = (1 - A) * B;
+	dp[1][0][0] = (1 - A) * (1 - B);
 
 	for (int i = 2; i < 19; i++) {
-
-		for (int j = 0; j <= i; j++) {
-			for (int k = 0; k <= i; k++) {
-				// a, b, 둘다 못넣은 경우
-				dp[i][j][k] += dp[i - 1][j][k] * (1 - a) * (1 - b);
-				if (j > 0) // a만 넣은 경우
-					dp[i][j][k] += dp[i - 1][j - 1][k] * a * (1 - b);
-				if (k > 0) // b만 넣은 경우
-					dp[i][j][k] += dp[i - 1][j][k - 1] * (1 - a) * b;
-				if (j > 0 && k > 0) // 둘다 넣은 경우
-					dp[i][j][k] += dp[i - 1][j - 1][k - 1] * a * b;
+		for (int a = 0; a <= i; a++) {
+			for (int b = 0; b <= i; b++) {
+				dp[i][a][b] += (1 - A) * (1 - B) * dp[i - 1][a][b];
+				if (a > 0) // a가 넣었을 때
+					dp[i][a][b] += A * (1-B) * dp[i - 1][a - 1][b];
+				if (b > 0) // b가 넣었을 때
+					dp[i][a][b] += (1-A) * B * dp[i - 1][a][b - 1];
+				if (a > 0 && b > 0) // a, b 둘다 넣었을 때
+					dp[i][a][b] += A * B * dp[i - 1][a - 1][b - 1];
 			}
 		}
 	}
 
-	for (int i = 0; i < 19; i++) {
-		for (int j = 0; j < 19; j++) {
-			if (isPrime(i) || isPrime(j))
-				result += dp[18][i][j];
+	for (int a = 0; a < 19; a++) {
+		for (int b = 0; b < 19; b++) {
+			if (isPrime(a) || isPrime(b))
+				result += dp[18][a][b];
 		}
 	}
 
 	cout << result << "\n";
-
 	return 0;
 }
