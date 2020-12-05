011b1520895b3c6f960aea10015ce7a705eec251
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-12-05 23:14:29 +0900

    Simulation

--- 백준/20061번 모노미노도미노 2/20061.cpp
+++ 백준/20061번 모노미노도미노 2/20061.cpp
@@ -11,6 +11,11 @@ using namespace std;
 int n, score, blockCnt;
 int sero[6][4], garo[4][6];
 
+// 블럭 놓기
+// 세로를 예로 들어서 
+// 1 ~ 6 행 중 이미 놓인 블럭이 있다면 해당 r - 1에 블럭 놓기
+// 만약 r이 그대로 0이라면 마지막까지 다 탐색해도 놓인 블럭이 없다는 의미 => 제일 마지막 줄에 놓기
+// 가로도 동일하게
 void putBlock(int t, int y, int x) {
 
 	int r, c;
@@ -88,6 +93,10 @@ void putBlock(int t, int y, int x) {
 
 }
 
+// 세로의 경우 
+// 블럭 1줄 제거하고, 그 줄부터 맨 위 줄까지 한칸씩 땡겨서 내리고, 맨 윗줄은 0으로 초기화
+// 단, i값을 ++ 해줘야 한칸 내린 줄부터 탐색할 수 있음
+// 가로도 동일
 void removeBlock() {
 
 	//sero
@@ -106,7 +115,7 @@ void removeBlock() {
 				sero[idx][j] = sero[idx - 1][j];
 			sero[0][j] = 0;
 		}
-		i++;
+		i++; // 한줄을 위에서 내렷기 때문에 다시 i++해줘서 그 행을 다시 탐색
 	}
 
 	//garo
@@ -128,6 +137,13 @@ void removeBlock() {
 		i++;
 	}
 }
+
+// 세로의 경우
+// 0, 1 행을 탐색하면서 블럭이 놓여있으면 카운트
+// 카운트 한 만큼 맨 아래줄에서 한칸씩 당기기
+// 동일하게 맨위는 0으로 초기화
+
+// 가로도 동일
 
 void removeFadeBlock() {
 	//sero
@@ -170,6 +186,7 @@ void removeFadeBlock() {
 
 }
 
+// 남은 블럭 수 가져오기
 void getBlockCnt() {
 	for (int i = 0; i < 6; i++)
 		for (int j = 0; j < 4; j++)
